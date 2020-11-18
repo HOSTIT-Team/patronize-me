0896881eb2be4219099092de2f5e01756cccab76
Mersad Ajanovic <34665379+flamerged@users.noreply.github.com>
2020-11-18 12:31:50 +0100

    removed validation for first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,5 @@ class User < ApplicationRecord
   has_many :reviews, through: :bookings
   has_one_attached :avatar
 
-  # validates :first_name, :last_name, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ }
   validates :bio, length: { maximum: 500 }
 end
