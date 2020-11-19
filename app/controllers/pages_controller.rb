13b33bc0300244fdcbe91ec2588af2bc5d4704e5
Vladimir Tudakov <vladimirtudakov@me.com>
2020-11-19 14:52:40 +0100

    added footer and some other feautures

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,24 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :contact]
-  
+
   def home
   end
 
   def contact
+  end
+
+  def index
+    @offers = policy_scope(Offer)
+    @offer = Offer.new
+  end
+
+  private
+
+  def find_offer
+    @offer = Offer.find(params[:id])
+  end
+
+  def offer_params
+    params.require(:offer).permit(:title, :category, :price, :description, :city, :delivery_type, :banner)
   end
 end
