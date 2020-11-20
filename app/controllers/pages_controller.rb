8e79490d1b5fefce817da8fe1ed50410d6f21edd
Mersad Ajanovic <34665379+flamerged@users.noreply.github.com>
2020-11-20 11:52:10 +0100

    unnecessary code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,19 +6,4 @@ class PagesController < ApplicationController
 
   def contact
   end
-
-  def index
-    @offers = policy_scope(Offer)
-    @offer = Offer.new
-  end
-
-  private
-
-  def find_offer
-    @offer = Offer.find(params[:id])
-  end
-
-  def offer_params
-    params.require(:offer).permit(:title, :category, :price, :description, :city, :delivery_type, :banner)
-  end
 end
