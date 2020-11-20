51e186f8b152536fd36985cc2b6b1c322fd0b7e9
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-20 05:16:02 +0100

    refactored seearch into simple form with strong params

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,12 +3,16 @@ class OffersController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    if params[:category].present? && !params[:location].present?
-      @offers = policy_scope(Offer.where(category: params[:category]))
-    elsif !params[:category].present? && params[:location].present?
-      @offers = policy_scope(Offer.where(city: params[:location]))
-    elsif params[:category].present? && params[:location].present?
-      @offers = policy_scope(Offer.where(city: params[:location]).where(category: params[:category]))
+    #raise
+    if params[:offer].present? && 
+      if offer_params[:category].empty? || offer_params[:city].empty? then @offers = policy_scope(Offer.all) end
+      if offer_params[:category].present? && !offer_params[:city].present?
+        @offers = policy_scope(Offer.where(category: offer_params[:category]))
+      elsif !offer_params[:category].present? && offer_params[:city].present?
+        @offers = policy_scope(Offer.where(city: offer_params[:city]))
+      elsif offer_params[:category].present? && offer_params[:city].present?
+        @offers = policy_scope(Offer.where(city: offer_params[:city]).where(category: offer_params[:category]))
+      end
     else
       @offers = policy_scope(Offer.all)
     end
